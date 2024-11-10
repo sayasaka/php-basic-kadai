3b0e5ce10ca569cdc11efcf3381f07171cc200a2
sayasaka <sayaendo.g26@icloud.com>
2024-11-10 15:31:43 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UFT-8">
+        <title>kadai_007</title>
+    </head>
+    
+    <body>
+        <p>
+
+        <?php
+        $array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        print_r($array);
+
+        ?>
+        </p>
+    </body>
+</head>
